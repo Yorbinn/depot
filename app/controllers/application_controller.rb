1a4579c5b803c73332d9f6240096efe7413e9e7d
Jorge <jorge.tll@hotmail.com>
2017-03-17 21:06:29 -0600

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,23 @@
 class ApplicationController < ActionController::Base
-  #before_action :authorize
-  #before_action :set_i18n_locale_from_params
+  before_action :authorize
+  before_action :set_i18n_locale_from_params
   
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-   #protected
+   protected
  
-    #def authorize
-     # if request.format == Mime[:html]
-      #  unless User.find_by(id: session[:user_id])
-       #   redirect_to login_url, notice: "Please log in"
-        #end
-      #else
-       # authenticate_or_request_with_http_basic do |username, password|
-        #  user = User.find_by(name: username)
-         # user && user.authenticate(password)
-        #end
-      #end
-    #end
+    def authorize
+      if request.format == Mime[:html]
+        unless User.find_by(id: session[:user_id])
+          redirect_to login_url, notice: "Please log in"
+        end
+      else
+        authenticate_or_request_with_http_basic do |username, password|
+          user = User.find_by(name: username)
+          user && user.authenticate(password)
+        end
+      end
+    end
 
     def set_i18n_locale_from_params
       if params[:locale]
